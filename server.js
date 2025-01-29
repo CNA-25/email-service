957d5ac107bd4150a34e07e2dd09ee79620a26d7
Vika41 <elersvic@arcada.fi>
2025-01-29 11:36:32 +0200

    Mail not sending

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ console.log(`Node.js ${process.version}.`)
 app.use(express.json())
 
 const checkKey = (req, res, next) => {
+    console.log("Checking key...")
     if (req.query.api_key !== process.env.JWT_SECRET) {
         return res.status(403).json({ error: "Auth failed." })
     }
@@ -24,6 +25,44 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', checkKey, async (req, res) => {
+    const to = req.body.to
+    const subject = req.body.subject || process.env.DEFAULT_SUBJECT
+    const body = req.body.body
+    const from = req.body.from
+
+    if (!to || !subject || !body || !from) {
+        return res.status(400).json({ message: "Missing required variable: from, to, subject, body.", request: req.body })
+    }
+
+    console.log(`Sending mail on ${process.env.MAIL_HOST}, to: ${to}, from: ${from}, subject: ${subject}.`)
+
+    let transporter = nodemailer.createTransport({
+        host: process.env.MAIL_HOST,
+        port: process.env.MAIL_PORT || 25,
+        tls: {
+            rejectUnauthorized: false
+        }
+    })
+
+    try {
+        let info = await transporter.sendMail({
+            from: from,
+            to: to,
+            subject: subject,
+            text: striptags(body),
+            html: body
+        })
+        console.log(`Mail sent: ${info.messageId}.`)
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({ error: "Mail not sent.", message: error.message })
+    }
+
+    res.send({ message: "Mail sent." })
+})
+
+app.post('/email', async (req, res) => {
+    console.log(req.body)
     const to = req.body.to
     const subject = req.body.subject || process.env.DEFAULT_SUBJECT
     const body = req.body.body
