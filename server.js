ba521851da40610271917922c575f70ad259a087
Vika <117091666+Vika41@users.noreply.github.com>
2025-02-10 13:14:07 +0200

    Started modifying /order

--- server.js
+++ server.js
@@ -2,44 +2,74 @@ const express = require('express')
 const app = express()
 const nodemailer = require('nodemailer')
 const striptags = require('striptags')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 const PORT = process.env.PORT || 8080
+const MAIL_PORT = process.env.MAIL_PORT || 25
 
 console.log(`Node.js ${process.version}.`)
 app.use(express.json())
 
 const checkKey = (req, res, next) => {
-    console.log("Checking key...")
-    if (req.query.api_key !== process.env.JWT_SECRET) {
-        return res.status(403).json({ error: "Auth failed." })
+    try {
+        const authHeader = req.headers['authorization']
+        console.log(`Authorize API key: ${authHeader}`)
+        const apiKey = authHeader?.split(' ')[1]
+
+        if (apiKey !== process.env.API_KEY) {
+            return res.status(403).json({ error: "Auth failed." })
+        }
+        next()
+
+    } catch (error) {
+        console.log(error.message)
+        return res.status(401).json({ error: "Authorization failed." })
     }
-    next()
+}
+
+const checkJwt = (req, res, next) => {
+    try {
+        const authHeader = req.headers['authorization']
+        console.log(`Authorize JWT: ${authHeader}`)
+        const token = authHeader?.split(' ')[1]
+
+        const JWT = jwt.verify(token, process.env.JWT_SECRET)
+        console.log(`Token authorized for user.`)
+
+        next()
+    } catch (error) {
+        console.log(error.message)
+        return res.status(401).json({ error: "Authorization failed." })
+    }   
 }
 
 app.get('/', (req, res) => {
-    return res.json({ msg: "Mailer." })
+    //return res.send(`
+        //<h1>Mailer Status: Online.</h1>
+    //`)
+    res.json({ msg: "Mailer." })
 })
 
-app.post('/', checkKey, async (req, res) => {
+let transporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT || 25,
+    tls: {
+        rejectUnauthorized: false
+    }
+})
+
+app.post('/', /*checkJwt,*/ async (req, res) => {
     const to = req.body.to
     const subject = req.body.subject || process.env.DEFAULT_SUBJECT
     const body = req.body.body
-    const from = req.body.from
+    const from = req.body.from || process.env.MAIL_FROM
 
     if (!to || !subject || !body || !from) {
         return res.status(400).json({ message: "Missing required variable: from, to, subject, body.", request: req.body })
     }
 
-    console.log(`Sending mail on ${process.env.MAIL_HOST}, to: ${to}, from: ${from}, subject: ${subject}.`)
-
-    let transporter = nodemailer.createTransport({
-        host: process.env.MAIL_HOST,
-        port: process.env.MAIL_PORT || 25,
-        tls: {
-            rejectUnauthorized: false
-        }
-    })
+    console.log(`Sending mail on ${process.env.MAIL_HOST}:${MAIL_PORT}, to: ${to}, from: ${from}, subject: ${subject}.`)
 
     try {
         let info = await transporter.sendMail({
@@ -58,26 +88,17 @@ app.post('/', checkKey, async (req, res) => {
     res.send({ message: "Mail sent." })
 })
 
-app.post('/email', async (req, res) => {
-    console.log(req.body)
+app.post('/newsletter', checkKey, async (req, res) => {
     const to = req.body.to
     const subject = req.body.subject || process.env.DEFAULT_SUBJECT
     const body = req.body.body
-    const from = req.body.from
+    const from = process.env.MAIL_FROM
 
-    if (!to || !subject || !body || !from) {
-        return res.status(400).json({ message: "Missing required variable: from, to, subject, body.", request: req.body })
+    if (!to || !subject || !body) {
+        return res.status(400).json({ message: "Missing required variable: to, subject, body.", request: req.body })
     }
 
-    console.log(`Sending mail on ${process.env.MAIL_HOST}, to: ${to}, from: ${from}, subject: ${subject}.`)
-
-    let transporter = nodemailer.createTransport({
-        host: process.env.MAIL_HOST,
-        port: process.env.MAIL_PORT || 25,
-        tls: {
-            rejectUnauthorized: false
-        }
-    })
+    console.log(`Sending newsletter on ${process.env.MAIL_HOST}:${MAIL_PORT}, to: ${to}, from: ${from}, subject: ${subject}.`)
 
     try {
         let info = await transporter.sendMail({
@@ -87,19 +108,106 @@ app.post('/email', async (req, res) => {
             text: striptags(body),
             html: body
         })
-        console.log(`Mail sent: ${info.messageId}.`)
+        console.log(`Newsletter sent: ${info.messageId}.`)
     } catch (error) {
         console.log(error)
-        return res.status(400).json({ error: "Mail not sent.", message: error.message })
+        return res.status(400).json({ error: "Newsletter not sent.", message: error.message })
     }
 
-    res.send({ message: "Mail sent." })
+    res.send({ message: "Newsletter sent." })
+})
+
+app.post('/order', /*checkJwt,*/ async (req, res) => {
+    const from = process.env.MAIL_FROM
+    const to = req.body.to
+    const subject = req.body.subject || process.env.DEFAULT_SUBJECT
+    const body = req.body.body
+
+    if (!to || !subject || !body) {
+        return res.status(400).json({ message: "Missing required variable: to, subject, body.", request: req.body })
+    }
+
+    console.log(`Sending order confirmation on ${process.env.MAIL_HOST}:${MAIL_PORT}, to: ${to}, from: ${from}, subject: ${subject}.`)
+
+    try {
+        let info = await transporter.sendMail({
+            from: from,
+            to: to,
+            subject: subject,
+            body: striptags(body),
+            html: body
+        })
+        console.log(`Order confirmation sent: ${info.messageId}.`)
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({ error: "Order confirmation not sent.", message: error.message })
+    }
+
+    res.send({ message: "Order confirmation sent." })
+})
+
+app.post('/invoicing', /*checkJwt,*/ async (req, res) => {
+    const from = process.env.MAIL_FROM
+    const to = req.body.to
+    const subject = req.body.subject || process.env.DEFAULT_SUBJECT
+    const body = req.body.body
+
+    if (!to || !subject || !body) {
+        return res.status(400).json({ message: "Missing required variable: to, subject, body.", request: req.body })
+    }
+
+    console.log(`Sending invoice on ${process.env.MAIL_HOST}:${MAIL_PORT}, to: ${to}, from: ${from}, subject: ${subject}.`)
+
+    try {
+        let info = await transporter.sendMail({
+            from: from,
+            to: to,
+            subject: subject,
+            body: striptags(body),
+            html: body
+        })
+        console.log(`Invoice sent: ${info.messageId}.`)
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({ error: "Invoice not sent.", message: error.message })
+    }
+
+    res.send({ message: "Invoice sent." })
+})
+
+app.post('/shipping', /*checkJwt,*/ async (req, res) => {
+    const from = process.env.MAIL_FROM
+    const to = req.body.to
+    const subject = req.body.subject || process.env.DEFAULT_SUBJECT
+    const body = req.body.body
+
+    if (!to || !subject || !body) {
+        return res.status(400).json({ message: "Missing required variable: to, subject, body.", request: req.body })
+    }
+
+    console.log(`Sending shipping details on ${process.env.MAIL_HOST}:${MAIL_PORT}, to: ${to}, from: ${from}, subject: ${subject}.`)
+
+    try {
+        let info = await transporter.sendMail({
+            from: from,
+            to: to,
+            subject: subject,
+            body: striptags(body),
+            html: body
+        })
+        console.log(`Shipping details sent: ${info.messageId}.`)
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({ error: "Shipping details not sent.", message: error.message })
+    }
+
+    res.send({ message: "Shipping details sent." })
 })
 
 app.listen(PORT, () => {
     try {
         console.log(`Running on port ${PORT}.`)
     } catch (error) {
-        res.status(500).json({ message: "Internal server error." })
+        res.status(500).json({ message: 'Internal server error.' })
     }
 })
