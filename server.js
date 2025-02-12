47eb18ccd6f457223ba7d71ec51ca3a1454d77cc
Vika41 <elersvic@arcada.fi>
2025-02-12 14:52:17 +0200

    Fixes issue #5, ready for testing

--- server.js
+++ server.js
@@ -118,10 +118,16 @@ app.post('/newsletter', checkKey, async (req, res) => {
 })
 
 app.post('/order', /*checkJwt,*/ async (req, res) => {
-    const from = process.env.MAIL_FROM
+    const from = req.body.from
     const to = req.body.to
     const subject = req.body.subject || process.env.DEFAULT_SUBJECT
     const body = req.body.body
+
+    //console.log(body.orderItems)
+
+    const message = JSON.stringify(body)
+
+    console.log(message)
 
     if (!to || !subject || !body) {
         return res.status(400).json({ message: "Missing required variable: to, subject, body.", request: req.body })
@@ -134,8 +140,8 @@ app.post('/order', /*checkJwt,*/ async (req, res) => {
             from: from,
             to: to,
             subject: subject,
-            body: striptags(body),
-            html: body
+            body: striptags(message),
+            html: message
         })
         console.log(`Order confirmation sent: ${info.messageId}.`)
     } catch (error) {
@@ -202,6 +208,35 @@ app.post('/shipping', /*checkJwt,*/ async (req, res) => {
     }
 
     res.send({ message: "Shipping details sent." })
+})
+
+app.post('/user', /*checkJwt,*/ async (req, res) => {
+    const from = process.env.MAIL_FROM
+    const to = req.body.to
+    const subject = req.body.subject || process.env.DEFAULT_SUBJECT
+    const body = req.body.body
+
+    if (!to || !subject || !body) {
+        return res.status(400).json({ message: "Missing required variable: to, subject, body.", request: req.body })
+    }
+
+    console.log(`Sending user info on ${process.env.MAIL_HOST}:${MAIL_PORT}, to: ${to}, from: ${from}, subject: ${subject}.`)
+
+    try {
+        let info = await transporter.sendMail({
+            from: from,
+            to: to,
+            subject: subject,
+            body: striptags(body),
+            html: body
+        })
+        console.log(`User info sent: ${info.messageId}.`)
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({ error: "User info not sent.", message: error.message })
+    }
+
+    res.send({ message: "User info sent." })
 })
 
 app.listen(PORT, () => {
